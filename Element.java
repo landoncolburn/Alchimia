a70227129239904b71e3bc05f9fd19a6e9c5bbdf
Landon Colburn <colburnlandon@gmail.com>
2019-12-12 14:07:41 -0600

    Cleaning up old commented out code

--- Element.java
+++ Element.java
@@ -16,7 +16,7 @@ public class Element extends JComponent{
   private static final long serialVersionUID = 42l;
 
   private BufferedImage img,img2,img3;
-  private ElementType type; //Creates inital Element Type
+  private ElementType type;
 
   private RescaleOp ro = new RescaleOp(1.1f, 10, null);
 
--- ElementType.java
+++ ElementType.java
@@ -18,47 +18,20 @@ public enum ElementType{
   SILICON("Silicon", 2, 1),
   SODA("Soda", 3, 1);
 
-// public enum ElementType{
-//   CARBON("Carbon", "resources/carbon.png"),
-//   HYDROGEN("Hydrogen", "resources/hydro.png"),
-//   OXYGEN("Oxygen", "resources/oxygen.png"),
-//   NITROGEN("Nitrogen", "resources/nitrogen.png"),
-//   HELIUM("Helium", "resources/helium.png"),
-//   SODIUM("Sodium", "resources/sodium.png"),
-//   ALUMINUM("Aluminum", "resources/aluminum.png"),
-//   POTASSIUM("Potassium", "resources/potassium.png"),
-//   CHLORINE("Chlorine", "resources/chlorine.png"),
-//   WATER("Water", "resources/water.png"),
-//   CARBONWATER("Carbonated Water", "resources/carbonwater.png"),
-//   AMMONIA("Ammonia", "resources/ammonia.png"),
-//   SALT("Salt", "resources/salt.png"),
-//   TONIC("Tonic", "resources/tonic.png"),
-//   ORGANIC("Organic", "resources/organic.png"),
-//   SUGAR("Sugar", "resources/sugar.png"),
-//   SODA("Soda", "resources/soda.png");
-
   private final String name;
   private final int spriteX;
   private final int spriteY;
-  // private final String link;
 
   ElementType(String name, int x, int y){
     this.name = name;
     this.spriteX = x;
     this.spriteY = y;
   }
-  // ElementType(String name, String link){
-  //   this.name = name;
-  //   this.link = link;
-  // }
 
   public String getName(){
     return name;
   }
 
-  // public String getLink(){
-  //   return link;
-  // }
 
   public int getY(){
     return spriteY*128;
